b1836a86f41e66fed6ee31f9b342c7fb5fe9bbc4
Zsombor Benke <r3dream3r@gmail.com>
2019-01-04 23:17:55 +0100

    Addig article orig requests

--- scraper.py
+++ scraper.py
@@ -5,3 +5,10 @@ import itertools
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
+
+headers = ({'User-Agent':
+            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+
+sapo = "https://casa.sapo.pt/Venda/Apartamentos/?sa=11&or=10"
+
+response = get(sapo, headers=headers)
